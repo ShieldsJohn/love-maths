4381b7e1aaebc0a559174b44b8df7435875e5a34
ShieldsJohn <>
2023-10-22 11:47:37 +0000

    added code to check the answer and respond to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function() {
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                alert("You clicked Submit!");
+                checkAnswer();
             } else {
                 let gameType = this.getAttribute("data-type");
                 runGame(gameType);
@@ -37,27 +37,42 @@ function runGame(gameType) {
 }
 
 /**
+ * checks the answer against the first element in
+ * the returned calculateCorrectAnswer array
+ */
+
+function checkAnswer(){
+   
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("You got it right big man!");
+    } else {
+        alert (`It's not ${userAnswer}. It's ${calculatedAnswer[0]}, ya fanny!`);
+    }
+
+    runGame(calculatedAnswer[1]);
+}
+
+/**
  * gets the operands (numbers) and the operator (plus, minus etc)
  * directly from the DOM and returns the correct answer
  */
 
-function checkAnswer(){
-    //parseInt returns an integer, rather than a string from the DOM
-    let operand1 = parseInt(document.getElementById('operand1').innerText);
-    let operand2 = parseInt(document.getElementById('operand2').innerText);
-    let operator = document.getElementById("operator").innerText;
-
-    if (operator === "+") {
-        return [operand1 + operand2, "addition"];
-    } else {
-        alert("Unimplemented operator ${operator}");
-        throw `Unimplemented operator ${operator}. Aborting!`;
-    }
-
-}
-
 function calculateCorrectAnswer(){
-
+     //parseInt returns an integer, rather than a string from the DOM
+     let operand1 = parseInt(document.getElementById('operand1').innerText);
+     let operand2 = parseInt(document.getElementById('operand2').innerText);
+     let operator = document.getElementById("operator").innerText;
+ 
+     if (operator === "+") {
+         return [operand1 + operand2, "addition"];
+     } else {
+         alert("Unimplemented operator ${operator}");
+         throw `Unimplemented operator ${operator}. Aborting!`;
+     }
 }
 
 function incrementScore(){
